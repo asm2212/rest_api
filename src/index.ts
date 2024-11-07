0494ecaaab68c3cd5c219f790b654196ebd9f88a
asm2212 <asmareadmasu0@gmail.com>
2024-11-07 10:11:43 +0300

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("heloo ts");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors(
+    {
+        credentials: true,
+    }
+));
+
+app.use(bodyParser.json());
+app.use(compression());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+server.listen(3000,()=> {
+    console.log("Server is running on port 3000");
+});
+
